a041ac31b9970273c6bb352f5a67fb7f5158b54a
Francisco Ribas <21309@columbushs.com>
2019-08-30 09:38:03 -0400

    First commit

---
+++ src/schedule/Schedule.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schedule;
+
+/**
+ *
+ * @author 21309
+ */
+public class Schedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = "Frankie Ribas";
+        int graduationYear = 2021;
+        System.out.println("My name is " + name + " and I'll graduate in " + graduationYear + ".");
+        
+        String course1 = "Hon English 11";
+        String course2 = "AP Computer Science A";
+        String course3 = "Visual Computer Modeling";
+        String course4 = "AP Physics 1";
+        String course5 = "Global Studies";
+        String course6 = "Hon Pre-Calculus";
+        String course7 = "Hon American History";
+        String teacher1 = "Cody Morrissey";
+        String teacher2 = "Matthew Larrubia";
+        String teacher3 = "Omar Delgado";
+        String teacher4 = "Rudy Sanchez";
+        String teacher5 = "Andres Gonzalez";
+        String teacher6 = "Wenceslao Ortega";
+        String teacher7 = "Sharifah Lee";
+        
+        System.out.println("+---------------------------------------------------+");
+        System.out.println("| 1 |             " + course1 + " |   " + teacher1 + " |");
+        System.out.println("| 2 |      " + course2 + " | " + teacher2 + " |");
+        System.out.println("| 3 |   " + course3 + " |     " + teacher3 + " |");
+        System.out.println("| 4 |               " + course4 + " |     " + teacher4 + " |");
+        System.out.println("| 5 |             " + course5 + " |  " + teacher5 + " |");
+        System.out.println("| 6 |           " + course6 + " | " + teacher6 + " |");
+        System.out.println("| 7 |       " + course7 + " |     " + teacher7 + " |");
+        System.out.println("+---------------------------------------------------+");
+    }
+    
+}
